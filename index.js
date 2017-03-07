05c330f670a1354bb405a44d1c4632549e024426
alex-ketch <alex@ketch.me>
2017-03-07 10:54:57 -0500

    Fix indentation

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ var inlineSource = require('inline-source').sync;
 
 var inline = function(options) {
   return function(files, metalsmith, done) {
-      var htmlFiles = Object.keys(files).filter(function(file) {
-        // Make sure the filename ends in either htm or html
-        var checkIfHtml = /[.](?:html?)$/.test(file);
+    var htmlFiles = Object.keys(files).filter(function(file) {
+      // Make sure the filename ends in either htm or html
+      var checkIfHtml = /[.](?:html?)$/.test(file);
 
-        if (checkIfHtml) {
-          return file;
-        }
-      });
+      if (checkIfHtml) {
+        return file;
+      }
+    });
 
-      htmlFiles.map(function(path) {
-        files[path].contents = new Buffer(inlineSource(files[path].contents.toString(), options)), 'utf8';
-      });
+    htmlFiles.map(function(path) {
+      files[path].contents = new Buffer(inlineSource(files[path].contents.toString(), options)), 'utf8';
+    });
     done();
   };
 };
